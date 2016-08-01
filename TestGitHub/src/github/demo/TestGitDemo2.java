b33005b8ec67c4a8d04d10946890d9fef1fe2798
cdewanga <cdewanga@DIN52003435.corp.capgemini.com>
2016-08-01 14:27:35 +0530

    2nd fie commit

--- TestGitHub/src/github/demo/TestGitDemo2.java
+++ TestGitHub/src/github/demo/TestGitDemo2.java
@@ -1,12 +1,10 @@
 package github.demo;
 
-public class TestGitHub {
+public class TestGitDemo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testing GIt HUB");
-		System.out.println("Testing GIt HUB1");
-		
+
 	}
 
 }
